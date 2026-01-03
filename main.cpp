36cc1cb703712467668c7a78b4bef54b4b757ce4
erzabeshiri <erza.beshiri@student.uni-pr.edu>
2026-01-03 22:27:27 +0100

    commit 1 and 2

--- main.cpp
+++ main.cpp
@@ -4,16 +4,41 @@ using namespace std;
 int llogaritNoten(int p);
 int merrPiket();
 
-int main(){
+int main()
+{
 
-  return 0;
+	return 0;
 }
 
-int llogaritNoten(int p){
-	if (p < 50) return 5;
-	return 5; 
+int llogaritNoten(int p)
+{
+	if (p < 50)
+	{
+		return 5;
+	}
+	else if (p < 60)
+	{
+		return 6;
+	}
+	else if (p < 70)
+	{
+		return 7;
+	}
+	else if (p < 80)
+	{
+		return 8;
+	}
+	else if (p < 90)
+	{
+		return 9;
+	}
+	else
+	{
+		return 10;
+	}
 }
 
-int merrPiket(){
-  return 0;
+int merrPiket()
+{
+	return 0;
 }
