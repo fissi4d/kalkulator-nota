e8fd187f292b916e91e7afeeec168fb2e683426d
erzabeshiri <erza.beshiri@student.uni-pr.edu>
2026-01-03 22:38:06 +0100

    Ndryshimi

--- main.cpp
+++ main.cpp
@@ -12,26 +12,25 @@ int main()
 	return 0;
 }
 
-<<<<<<< HEAD
 int llogaritNoten(int p)
 {
 	if (p < 50)
 	{
 		return 5;
 	}
-	else if (p < 60)
+	else if (p < 65)
 	{
 		return 6;
 	}
-	else if (p < 70)
+	else if (p < 80)
 	{
 		return 7;
 	}
-	else if (p < 80)
+	else if (p < 90)
 	{
 		return 8;
 	}
-	else if (p < 90)
+	else if (p < 100)
 	{
 		return 9;
 	}
@@ -43,33 +42,29 @@ int llogaritNoten(int p)
 
 int merrPiket()
 {
-	return 0;
-=======
-int llogaritNoten(int p){
-	if (p < 0) return false;
-	return 5; 
+	int p;
+	cout << "Shkruani numrin e pikeve: ";
+	cin >> p;
+	return p;
+}
+bool validoPiket(int piket)
+{
+	if (piket >= 0 && piket <= 100)
+	{
+		return true;
+	}
+	return false;
 }
 
-int merrPiket(){
-  int p;
-  cout<<"Shkruani numrin e pikeve: ";
-  cin>>p;
-  return p;
-}
-bool validoPiket(int piket) {
-    if (piket >= 0 && piket <= 100) {
-        return true;
-    }
-    return false;
-}
-
-void shfaqRezultatin(int n){
+void shfaqRezultatin(int n)
+{
 	cout << "Nota juaj eshte: " << n << endl;
-	if (nota == 5) {
+	if (nota == 5)
+	{
 		cout << "Rezultati: Ngel - Nota 5" << endl;
 	}
-	else {
+	else
+	{
 		cout << "Rezultati: Kaloi - Nota " << n << endl;
 	}
->>>>>>> main
 }
